240feff4f59987ea501601decf779254345c4363
kukuhbaiquni <kukuh.baiquni@gmail.com>
2023-11-14 16:14:48 +0700

    Fix attempt mobile view broken

--- index.php
+++ index.php
@@ -37,18 +37,37 @@
     <title>Chat Page New</title>
     <style type="text/css">
        #main-window {
-				width: 100% !important;
-				height: 100vh !important;
-				left: 0 !important;
-				top: 0 !important;
-				border-radius: 0 !important;
-				display: block;
-				transform: translateY(0);
-				opacity: 1 !important;
-			}
+            width: 100% !important;
+            height: 100vh !important;
+            left: 0 !important;
+            top: 0 !important;
+            border-radius: 0 !important;
+            display: block;
+            transform: translateY(0);
+            opacity: 1 !important;
+        }
+        #ln-chat-scroller {
+            height: calc(100% - 190px);
+        }
+        #ln-caption-bubble, #ln-bubble-trigger {
+            display: none;
+        }
     </style>
 </head>
 <body>
+    <script src="/mobile-detect.min.js"></script>
+		<script>
+			const device = new MobileDetect(window.navigator.userAgent);
+			
+			if (device.mobile()) {
+				const inter = setInterval(() => {
+					const target = document.getElementById('ln-chat-scroller');
+					if (target) {
+						target.style.height = 'calc(100% - 245px)'
+					}
+				}, 100)
+			}
+		</script>
     <script>
 		var lennawebchat = document.createElement('script'); lennawebchat.src = "https://v3.lenna.ai/chat/lenna-init.js";var app = document.createElement('script');app.src = "https://v3.lenna.ai/chat/app.js";document.head.prepend(lennawebchat);document.head.prepend(app);lennawebchat.onload = function () {LennaWebchatInit('lejRej','9aAOdv')}; 
     </script>
